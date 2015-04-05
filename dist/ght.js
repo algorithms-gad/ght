7a3cfe69fc9267fd95c17d038d15be9fec3c2f3a
Touhid Arastu <touhid.arastu@gmail.com>
2015-04-05 16:21:20 +0430

    change version

--- dist/ght.js
+++ dist/ght.js
@@ -22,7 +22,7 @@
   }
 
   // Current version.
-  ght.VERSION = '2.1.1';
+  ght.VERSION = '2.1.2';
 
   //English and Persian characters
   var enChars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
